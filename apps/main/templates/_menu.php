1d21cad8290f9f83be9495883c419b3b976111ff
rtatar <rtatar@dddfb7a6-776b-11e1-8be5-dbd00ea39e35>
2012-05-19 12:38:08 +0000

    meny i css

--- apps/main/templates/_menu.php
+++ apps/main/templates/_menu.php
@@ -1,11 +1,26 @@
 <?php
 $menu = array(
-  'Start' => array(
-    'url' => 'home/index'
+  'Ready4cloud' => array(
+    'url' => '@homepage'
+  ),
+  'Profil' => array(
+    'url' => '@profilepage'
   ),
   'Serwery' => array(
     'url' => 'server/index'
-  )
+  ),
+  'Dyski' => array(
+    'url' => 'drive/index'
+  ),
+  'Firewall' => array(
+    'url' => 'firewall/index'
+  ),
+  'Loadbalancer' => array(
+    'url' => 'loadbalance/index'
+  ),
+  'Platnosci' => array(
+    'url' => 'payment/index'
+  ),
 );
 ?>
 <ul id="menu">
