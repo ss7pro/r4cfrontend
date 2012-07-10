f0b3e2cbfd49e44610a4ad550cdd64d13bcabfa0
rtatar <rtatar@dddfb7a6-776b-11e1-8be5-dbd00ea39e35>
2012-07-10 19:07:38 +0000

    billing module

---
+++ apps/main/modules/billing/actions/actions.class.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * billing actions.
+ *
+ * @package    ready4cloud
+ * @subpackage billing
+ * @author     Roman Tatar <romantatar@gmail.com>
+ * @version    SVN: $Id: actions.class.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
+ */
+class billingActions extends sfActions
+{
+  public function executeIndex(sfWebRequest $request)
+  {
+  }
+}
