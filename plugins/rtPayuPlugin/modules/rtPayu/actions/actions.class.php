6b8b4667ff5aa1621ad55ebb5cbdeacf1c1a3302
rtatar <rtatar@dddfb7a6-776b-11e1-8be5-dbd00ea39e35>
2012-07-26 13:00:13 +0000

    transaction status notification

--- plugins/rtPayuPlugin/modules/rtPayu/actions/actions.class.php
+++ plugins/rtPayuPlugin/modules/rtPayu/actions/actions.class.php
@@ -1,6 +1,6 @@
 <?php
 
-require_once(dirname(__FILE__) . '/../lib/BasesfGuardAuthActions.class.php');
+require_once(dirname(__FILE__) . '/../lib/BasertPayuActions.php');
 
 /**
  *
--- plugins/rtPayuPlugin/modules/rtPayu/lib/BasertPayuActions.php
+++ plugins/rtPayuPlugin/modules/rtPayu/lib/BasertPayuActions.php
@@ -1,7 +1,15 @@
 <?php
-class BasrrtPayuActions extends sfActions
+class BasertPayuActions extends sfActions
 {
   public function executeNotify(sfWebRequest $request)
   {
+    $event = rtPayuTransactionEvent::createFromRequest($request);
+    $this->getLogger()->log('PAYMENT NOTIFY: ' . (string)$event);
+
+    if(!$event->isValid()) return $this->renderText('FAIL');
+
+    //TODO: fetch transaction status here and store in log
+
+    return $this->renderText('OK');
   }
 }
